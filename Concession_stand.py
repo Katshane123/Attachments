2782c125b379a3f602b85656825a7430019834a3
kat <151824405+Katshane123@users.noreply.github.com>
2025-11-19 19:54:32 +0200

    Add files via upload

---
+++ Concession_stand.py
@@ -0,0 +1,12 @@
+
+Numbers = [1 , 5 , 8 , 10, 60, 34 ,34, 32, 21 , 90, 65, 74, 56, 44, 99, 63,]
+Uniques = []
+
+for number in Numbers: 
+    if number not in Uniques : 
+        Uniques.append(number)
+
+print(Uniques)
+
+
+
