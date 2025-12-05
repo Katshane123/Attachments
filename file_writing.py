f8bd684c5c68bfee3e0b942b8e66b1a6d1e87fe9
kat <151824405+Katshane123@users.noreply.github.com>
2025-12-05 23:51:22 +0200

    file_writing.py

--- file_writing.py
+++ file_writing.py
@@ -3,8 +3,9 @@
 txt_data = "I love pizza actually"
 
 file_path = "output.txt"
-
+# Use different modes like w(write) , r (read)
 with open(file= file_path, mode= "w") as file:
     file.write(txt_data)
+
 
     print(F"txt file '{file_path}' has been created")
